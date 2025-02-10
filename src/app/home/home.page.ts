ea10ede720eb7f40c282fca6be8696e3b0773238
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-10 13:34:55 -0300

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,24 @@ export class HomePage {
 
   constructor() {}
 
+  number1 = 0;
+  number2 = 0;
+  result = 0;
+
+  add() {
+    this.result=this.number1 + this.number2;
+  }
+
+  subtract() {
+    this.result=this.number1 - this.number2;
+  }
+
+  divide() {
+    this.result=this.number1 / this.number2;
+  }
+
+  multiply() {
+    this.result=this.number1 * this.number2;
+  }
+
 }
